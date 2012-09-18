6d89e2f4a2fa62466d7024dd30ddb3a4dbccad18
Alexander Knapstein <alexander.knapstein@inspirationlabs.com>
2012-09-18 14:59:37 +0200

    rel. 0.1.1
    fixed recursive validator within fieldsets
    
    Signed-off-by: Alexander Knapstein <alexander.knapstein@inspirationlabs.com>

---
+++ package.js
@@ -0,0 +1,23 @@
+{
+  "name": "node-forms",
+  "description": "node.js library for creating, rendering and validating forms and their respective elements",
+  "main": "./index",
+  "author": "INSPIRATIONlabs",
+  "version": "0.1.1",
+  "repository": {
+      "type": "git",
+      "url": "https://github.com/INSPIRATIONlabs/node-forms.git"
+  },
+  "bugs": {
+      "url": "https://github.com/INSPIRATIONlabs/node-forms/issues"
+  },
+  "dependencies": {
+      "async": "0.1.22"
+  },
+  "licenses": [
+      {
+          "type": "MIT",
+          "url": "http://"
+      }
+  ]
+}
